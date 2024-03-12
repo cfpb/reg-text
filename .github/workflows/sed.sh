b8cd90212063b70808ec33f962e1de4a9b0e2c48
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-03-12 10:00:11 -0400

    Collapse around hyphen

--- .github/workflows/sed.sh
+++ .github/workflows/sed.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-sed -e '/^ *$/d'     `# Whitespace-only or empty lines` \
-    -e 's/ / /g'     `# nbsp to space` \
-    -e 's/–/-/g'     `# endash to hyphen` \
-    -e "s/’/'/g"     `# apostrophe to single quote` \
-    -e 's/\s—\s/—/g' `# collapse whitespace around emdash`
+sed -E\
+    -e '/^ *$/d' `# Whitespace-only or empty lines` \
+    -e 's/ / /g' `# nbsp to space` \
+    -e 's/–/-/g' `# endash to hyphen` \
+    -e "s/’/'/g" `# apostrophe to single quote` \
+    -e 's/[[:blank:]]([—-])[[:blank:]]/\1/g' `# collapse whitespace around emdash and hyphen`
