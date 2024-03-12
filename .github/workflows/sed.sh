4d7456b5c2b5e1407bb43fb1aa2be06089e66bf9
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-03-12 10:57:32 -0400

    all dashes to hyphen

--- .github/workflows/sed.sh
+++ .github/workflows/sed.sh
@@ -3,6 +3,6 @@
 sed -E\
     -e '/^ *$/d' `# Whitespace-only or empty lines` \
     -e 's/ / /g' `# nbsp to space` \
-    -e 's/–/-/g' `# endash to hyphen` \
+    -e 's/[—–]/-/g' `# em- and en-dash to hyphen` \
     -e "s/’/'/g" `# apostrophe to single quote` \
-    -e 's/[[:blank:]]([—-])[[:blank:]]/\1/g' `# collapse whitespace around emdash and hyphen`
+    -e 's/[[:blank:]]-[[:blank:]]/\1/g' `# collapse whitespace around hyphen`
